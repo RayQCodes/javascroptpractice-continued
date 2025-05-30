4d03e434c78903415f222d0a2b5d30f240e36c5c
RayQCodes <rquan3@gmu.edu>
2025-05-30 18:39:43 -0400

    arrows

--- arrows.js
+++ arrows.js
@@ -1,45 +1,38 @@
 //arrow functions:
-const pragalv = () => console.log("pragalvs the boss");
-pragalv();
 
 //2 create arrow funcrion takes in name as parameter and hello and returns the greeting
 
-const hello = (name, age) => {
-  console.log("pragalvy");
-  console.log(`hello nice to meet you ${name} Im ${age} years old`);
-};
-hello("PragalvTheWarlord", 25);
+const hello = (name, age) =>
+  console.log(`hello ${name} and im ${age} years old`);
+
+hello("PragalvTheMassa", 20);
 
 //create and use a higher order function to set a 5 second delay to run the inner function to say pragalvs current title after 5 seconds
-setTimeout(() => {
-  console.log("Pragalv is Senior Dev");
-}, 5000);
+
+setTimeout(() => console.log("hello my boy"), 3000);
 
 //math reduce and filter on a list or array  and map()
 
+numbers = [1, 2, 3, 4, 5];
+
 //using map functions
 //created two new lists squares and cubes and stored squares and cubed values of numbers into the new arrays
-let numbers = [1, 2, 3, 4, 5];
-let squarefunction = numbers.map((num) => Math.pow(num, 2));
-console.log(squarefunction);
 
-let cubes = numbers.map((num) => Math.pow(num, 3));
-console.log(cubes);
+const square = numbers.map((num) => Math.pow(num, 2));
+console.log(square);
 
+const cube = numbers.map((num) => Math.pow(num, 3));
+console.log(cube);
 //use the  filter method to filter out any odd numbers within numbers array
-
-const odd = numbers.filter((num) => num % 2 !== 0);
-console.log(odd);
-
-const even = numbers.filter((num) => {
-  if (num % 2 === 0) {
+//ima a baka
+const odd = numbers.filter((num) => {
+  if (num % 2 !== 0) {
     return num;
   }
 });
-console.log(even);
+
+console.log(odd);
 
 //use .reduce to find the total amount of numbers in the numbers array
-
-const total = numbers.reduce((acc, num) => acc + num, 0);
-
-console.log(total);
+const reduce = numbers.reduce((sum, number) => sum + number, 0);
+console.log(reduce);
