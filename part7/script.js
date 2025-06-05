6f34932581935673ed6ba99673c3265adcddd6f2
RayQCodes <rquan3@gmu.edu>
2025-06-05 11:30:22 -0400

    updated coding 6/5/2025

---
+++ part7/script.js
@@ -0,0 +1,3 @@
+console.log("Hello from script");
+console.log(window); //can use window object to see all properties by the browser .
+//window.outerHeight; //done in browser shows u windows outer height
