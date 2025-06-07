69a9ba3d972abac860ca68268f2ac319e98e4908
RayQCodes <rquan3@gmu.edu>
2025-06-07 10:59:30 -0400

    updates

--- part8/script.js
+++ part8/script.js
@@ -15,3 +15,31 @@ highlightbutton.addEventListener("click", function () {
 });
 
 //example 3
+const orderbutton = document.getElementById("changeOrder");
+
+orderbutton.addEventListener("click", function () {
+  let parentclass = document.getElementById("coffeeOrder");
+  parentclass.children[0].textContent = "Espresso";
+
+  parentclass.children[0].style.backgroundColor = "brown";
+  parentclass.children[0].style.padding = "10px";
+});
+
+//example 4
+const button4 = document.getElementById("addNewItem");
+
+button4.addEventListener("click", function () {
+  let newitem = document.createElement("li");
+  newitem.textContent = "Eggs";
+  let parent4 = document.getElementById("shoppingList");
+  parent4.appendChild(newitem);
+});
+
+//example5:
+const removebutton = document.getElementById("removeLastTask");
+
+removebutton.addEventListener("click", function () {
+  let list = document.getElementById("taskList");
+  //list.removeChild[2];
+  list.children[2].remove();
+});
